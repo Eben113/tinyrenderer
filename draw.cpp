656c916ddcf5ef94bbd184d8eaf284e846f0b2b5
Eben113 <ebenezeroyebola2004@gmail.com>
2026-07-02 15:49:07 +0100

    scanline rendering (my attempt)

--- draw.cpp
+++ draw.cpp
@@ -11,6 +11,11 @@ void swap(int& a, int& b){
 
 
 void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color, bool draw, std::vector<std::array<int,2>>* points){
+    if(ay == by){
+        if(ax > bx){swap(ax,bx);}
+        for(int x = ax; x<=bx; x++){framebuffer.set(x, ay, color);}
+        return;
+    }
     bool steep = false;
     int dy = abs(by - ay), dx = abs(bx - ax);
 
@@ -30,10 +35,10 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color,
     int E = 0, y = ay;
     std::vector<std::array<int,2>>& res = *points;
 
-    for(int x = ax; x < bx; x++){
+    for(int x = ax; x <= bx; x++){
         if(!draw){
 
-            std::cout << res.size() << "\n";
+
             if(steep){
                 std::array<int,2> p = {y,x};
                 res.push_back(p);
@@ -42,7 +47,6 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color,
                 std::array<int,2> p = {x,y};
                 res.push_back(p);
             }
-            std::cout << "here" << "\n";
         }
 
         else{
@@ -57,44 +61,50 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color,
 
 }
 
+void drawVerts(std::vector<std::array<int, 2>>& l1, std::vector<std::array<int, 2>>& l2, int start, TGAImage &framebuffer, TGAColor color){
+    int step1 = l1[0][1] == start? 1 : -1;
+    int step2 = l2[0][1] == start? 1: -1;
+    int stop1 = step1 == 1? l1.size() - 1 : 0;
+    int stop2 = step2 == 1? l2.size() - 1 : 0;
+    int pointer1 = (step1 == 1)? 0: l1.size()-1, pointer2 = (step2 == 1)? 0: l2.size()-1;
+    int height = std::abs(l1[0][1] - l1[l1.size()-1][1]);
 
-void triangle(int x1, int x2, int x3, int y1, int y2, int y3, TGAImage &framebuffer, TGAColor color){
-    int x[3] = {x1, x2, x3};
-    int y[3] = {y1, y2, y3};
-
-
-    for(int i = 0; i<2; i++){
-        if(x[i] > x[i+1]){
-            swap(x[i], x[i+1]);
-            swap(y[i], y[i+1]);
-        }
+    for(int y = start; y< start+height; y++){
+        while((l1[pointer1][1] == y) && (pointer1 != stop1)){pointer1 += step1;}
+        while((l2[pointer2][1] == y) && (pointer2 != stop2) ){pointer2 += step2;}
+        std::cout << pointer1 << "  " << pointer2 << "\n";
+        draw::line(l1[pointer1-step1][0], y, l2[pointer2-step2][0], y, framebuffer, color);
     }
-    std::cout << x[0] << "  " << x[1] << "  " << x[2] << "\n";
+}
+
+void triangle(int x0, int x1, int x2, int y0, int y1, int y2, TGAImage &framebuffer, TGAColor color){
+    if(y0>y1){swap(x0, x1); swap(y0, y1);}
+    if(y0>y2){swap(x0, x2); swap(y0, y2);}
+    if(y1>y2){swap(x1, x2); swap(y1, y2);}
 
 
-    std::vector<std::array<int, 2>> line1, line2;
 
-    line(x[0], y[0], x[1], y[1], framebuffer, color, false, &line1);
-    line(x[1], y[1], x[2], y[2], framebuffer, color, false, &line2);
-    bool asc1 = true, asc2 = true;
+    std::vector<std::array<int, 2>> line0, line1, line2, line3;
 
-    if(line1[line1.size()-1]<line1[0]){asc1 = false;}
-    if(line2[line2.size()-1]<line2[0]){asc2 = false;}
+    line(x0, y0, x1, y1, framebuffer, color, false, &line0);
+    line(x0, y0, x2, y2, framebuffer, color, false, &line1);
+    line(x1, y1, x2, y2, framebuffer, color, false, &line2);
 
-    std::cout << line1.size() << "  " << line2.size() << "\n";
-    
-    int i = 0, a, b;
-    std::array<int,2> p1;
-    std::array<int,2> p2;
-    
-    while((i<line1.size()) || (i<line2.size())){
-        if(i<line1.size()){p1 = asc1? line1[line1.size()-i-1]: line1[i];}
-        if(i<line2.size()){p2 = asc2? line2[i] : line2[line2.size()-i-1];}
-        std::cout << p1[0] << "  " << p1[1] << "  " << p2[0] << "  " << p2[1] << "\n";
-        line(p1[0], p1[1], p2[0], p2[1], framebuffer, color);
-        i++;
+    line(x0, y0, x1, y1, framebuffer, color);
+    line(x0, y0, x2, y2, framebuffer, color);
+    line(x1, y1, x2, y2, framebuffer, color);
 
+    int index = 0;
+    for(auto [x,y] : line1){
+        if(y != y1){index ++;}
+        else{break;}
     }
+    line3 = std::vector<std::array<int, 2>>(line1.begin()+index, line1.end());
+    line1.erase(line1.begin()+index, line1.end());
+    if(line1[0][1] > line3[line3.size()-1][1]){std::swap(line1, line3);}
+
+    drawVerts(line0, line1, y0, framebuffer, color);
+    drawVerts(line2, line3, y1, framebuffer, color);
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -15,7 +15,11 @@ int main(int argc, char** argv) {
     
     TGAImage framebuffer = TGAImage(300, 300, TGAImage::RGB);
     draw::line(30, 30, 60, 60, framebuffer, blue);
-    draw::triangle(7, 45, 100, 100, 120,  60, framebuffer, red);
+    draw::triangle(30, 45, 100, 100, 120,  60, framebuffer, red);
+
+    framebuffer.set(30, 100, white);
+    framebuffer.set(45, 120, white);
+    framebuffer.set(100, 60, white);
 
     framebuffer.write_tga_file("triangle.tga");
     return 0;
